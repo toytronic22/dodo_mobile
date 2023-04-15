232e507eee17fa518c50dcd1902324ee23c94cc4
toytronic22 <leshaklew@gmail.com>
2023-04-15 21:59:13 +0300

    1st commit

--- src/test/java/tests/browserstack/DodoProductTest.java
+++ src/test/java/tests/browserstack/DodoProductTest.java
@@ -6,21 +6,21 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
+
+@Tag("browserstack")
 public class DodoProductTest extends TestBase {
-        @Tag("browserstack")
-        @DisplayName("Checking text in due order")
-        @Test
-        @Owner("toytronic")
-        void checkButtonLogIn() {
-            step("Click on the NavBar Menu button", () -> {
-                $(id("org.wikipedia.alpha:id/menu_overflow_button")).click();
-            });
-        }
+    @DisplayName("Checking text in due order")
+    @Test
+    @Owner("toytronic")
+    void checkButtonLogIn() {
+
+        step("Click on Combo button", () -> {
+            $(id("ru.dodopizza.app:id/Комбо")).click();
+        });
+
     }
+}
