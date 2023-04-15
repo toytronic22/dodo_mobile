ce4cf748137a2a6e1f614670a1b3ea0c454afbee
toytronic22 <leshaklew@gmail.com>
2023-04-15 22:09:28 +0300

    1st commit

--- src/test/java/tests/browserstack/DodoProductTest.java
+++ src/test/java/tests/browserstack/DodoProductTest.java
@@ -9,6 +9,7 @@ import tests.TestBase;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.xpath;
 
 
 @Tag("browserstack")
@@ -19,7 +20,7 @@ public class DodoProductTest extends TestBase {
     void checkButtonLogIn() {
 
         step("Click on Combo button", () -> {
-            $(id("ru.dodopizza.app:id/Комбо")).click();
+            $(xpath("//android.widget.LinearLayout[@content-desc=\"Комбо\"]")).click();
         });
 
     }
